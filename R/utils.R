ea4a93ba5a2178190c2c20a14f244559889ca876
FeiQin <68352557+FeiQin92@users.noreply.github.com>
2021-10-04 17:33:06 -0400

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -11,25 +11,6 @@ logistic <- function(x, x0, k) {
     1 / (1 + exp(-k * (x - x0)))
 }
 
-#' Bind rows (matched)
-#'
-#' Bind the rows of two data frames, keeping only the columns that are common
-#' to both.
-#'
-#' @param df1 first data.frame to bind.
-#' @param df2 second data.frame to bind.
-#'
-#' @return data.frame containing rows from \code{df1} and \code{df2} but only
-#'         common columns.
-rbindMatched <- function(df1, df2) {
-    common.names <- intersect(colnames(df1), colnames(df2))
-    if (length(common.names) < 2) {
-        stop("There must be at least two columns in common")
-    }
-    combined <- rbind(df1[, common.names], df2[, common.names])
-
-    return(combined)
-}
 
 #' Bring items forward
 #'
@@ -56,39 +37,4 @@ bringItemsForward <- function(ll, items) {
     }
 
     return(ll)
-}
-
-#' Winsorize vector
-#'
-#' Set outliers in a numeric vector to a specified percentile.
-#'
-#' @param x Numeric vector to winsorize
-#' @param q Percentile to set from each end
-#'
-#' @return Winsorized numeric vector
-winsorize <- function(x, q) {
-
-    checkmate::check_numeric(x, any.missing = FALSE)
-    checkmate::check_number(q, lower = 0, upper = 1)
-
-    lohi <- stats::quantile(x, c(q, 1 - q), na.rm = TRUE)
-
-    if (diff(lohi) < 0) { lohi <- rev(lohi) }
-
-    x[!is.na(x) & x < lohi[1]] <- lohi[1]
-    x[!is.na(x) & x > lohi[2]] <- lohi[2]
-
-    return(x)
-}
-
-#' Calculate coefficient of variation
-#'
-#' Implementation of the coefficient of variation
-#'
-#' @param x vector of values.
-#'
-#' @return Value of coefficient of variation for vector
-#' @importFrom stats sd
-co.var <- function(x) {
-    sd(x) / mean(x)
 }
